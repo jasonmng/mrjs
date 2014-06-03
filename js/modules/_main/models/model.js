5e9312f4a618ec9153d886ce5b3f04380f05bb88
Jason <email_jsn@yahoo.com>
2014-06-03 13:18:52 -0400

    Structural changes 	- layout object is new a true marionette layout object 	- Application changes are pulled out from the module and moved into the driver 	- Application module definitions now occur in the driver file

--- js/modules/_main/models/model.js
+++ js/modules/_main/models/model.js
@@ -1,8 +1,9 @@
 'use strict';
 define(['backbone'], function(Backbone){
 
-	// Todo Model
-	// ----------
+   /* Todo Model
+    *--------------*/
+   
 	return Backbone.Model.extend({
 		defaults: {
 			title: '',
